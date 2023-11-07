02cd2e203da6ed03f76ea53e4e1798cd815eb6dc
ArchLance <lancee_zhang@163.com>
2023-11-07 18:03:28 +0800

    [TrafficGatherring]: fix bug in main.py
    Signed-off-by: ArchLance <lancee_zhang@163.com>

--- TrafficGathering/main.py
+++ TrafficGathering/main.py
@@ -1,5 +1,5 @@
 import yaml
-import json
+import pickle
 import pyshark
 from kafka import KafkaProducer
 
@@ -11,20 +11,20 @@ def init_config(config_file):
 
 def main():
     # 获取配置
-    args_config = init_config('./configs.yaml')
+    args_config = init_config('../config.yaml')
     # 设置消息队列
     producer = KafkaProducer(bootstrap_servers=args_config['mq']['server'])
-    topic = args_config['mq']['topic']
+    topic = args_config['mq']['traffic_topic']
     # 设置监听器
     capture = pyshark.LiveCapture(
-        interface=args_config['traffic_gethering']['interface'],
-        bpf_filter=['traffic-gethering']['traffic_gethering']
+        interface=args_config['traffic_gathering']['interface'],
+        bpf_filter=args_config['traffic_gathering']['capture_filter']
     )
     capture.sniff_continuously(packet_count=None)
     # 获取流量包
-    for i, pkt in enumerate(capture):
-        message = json.dumps(pkt)
-        producer.send(topic , message.encode('utf-8'))
+    for pkt in capture:
+        message = pickle.dumps(pkt)
+        producer.send(topic, message)
 
 if __name__ == '__main__':
     main()
