6a19c707ea02cc332204db4aa8895d0eec90adfc
dachilles <dachilles0112@gmail.com>
2023-11-11 02:31:09 +0800

    add: ddos traffic analyzer

---
+++ TrafficAnalyzer/abnomal-traffic/main.py
@@ -0,0 +1,42 @@
+import yaml
+from multiprocessing import Process
+from kafka import KafkaProducer, KafkaConsumer
+
+from ddos import detect
+
+# 解析配置
+def init_config(config_file):
+    with open(config_file, 'r') as f:
+        config = yaml.load(f, Loader=yaml.Loader)
+        return config
+
+def main():
+    # 获取配置
+    args_config = init_config('/home/dachilles/Workspace/NSSA_system/NSSA_Traffic/TrafficAnalyzer/config.yaml')
+    # 创建进程池
+    processes = []
+    # ddos 
+    # 消息队列设置
+    ddos_consumer = KafkaConsumer(args_config['mq']['traffic_topic'],
+                         group_id=args_config['mq']['ddos_group_id'],
+                         bootstrap_servers=args_config['mq']['bootstrap_servers']
+                         )
+    ddos_producer = KafkaProducer(bootstrap_servers=args_config['mq']['bootstrap_servers'])
+    # 创建对象
+    ddos_detector = detect.DDoS_Detector(traffic_consumer=ddos_consumer, 
+                             event_producer=ddos_producer, 
+                             topic=args_config['mq']['event_topic'],
+                             model_path=args_config['abnormal_traffic']['ddos']['model'],
+                             encoder_path=args_config['abnormal_traffic']['ddos']['encoder']
+                             )
+    processes.append(Process(target=ddos_detector.detect, args=(1000,)))
+
+
+
+    # 开始进程
+    for p in processes:
+        p.start()
+
+
+if __name__ == '__main__':
+    main()
