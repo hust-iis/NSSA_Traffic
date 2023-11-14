98b33a2c51108cb642133a79224ff580039595eb
xp <1157760794@qq.com>
2023-11-15 00:29:44 +0800

    fix trojan bugs

--- TrafficAnalyzer/abnomal_traffic/trojan/detect.py
+++ TrafficAnalyzer/abnomal_traffic/trojan/detect.py
@@ -21,14 +21,13 @@ from msg_models.models import AbnormalFlowModel, FLOW_TYPE_TROJAN
 
 class Trojan_Detector:
     # 初始化：配置项
-    def __init__(self, test_path, model_path, traffic_consumer, event_producer, topic) -> None:
+    def __init__(self, model_path, traffic_consumer, event_producer, topic) -> None:
         # 消息队列
         self.MQ_Traffic = traffic_consumer
         self.MQ_Event = event_producer
         self.MQ_Event_Topic = topic
 
         # 目录
-        self.test_path = test_path
         self.model_path = model_path
 
     def checkTrojan(self, filepath):
@@ -38,7 +37,7 @@ class Trojan_Detector:
 
         try:
             info_dictionaries = []
-            labelCsvFile = "./Trojan/Data/%s.csv" % label
+            labelCsvFile = "./abnomal_traffic/trojan/Data/%s.csv" % label
 
             if os.path.isdir(input):
                 input_dir = input
@@ -69,7 +68,7 @@ class Trojan_Detector:
             # print("Finish cleaning ...")
 
             # 读取数据处理的中转文件
-            labelCsvFile = "./Trojan/Data/%s.csv" % label
+            labelCsvFile = "./abnomal_traffic/trojan/Data/%s.csv" % label
             df = pd.read_csv(labelCsvFile, low_memory=False)
 
             # 进行数据处理
@@ -132,11 +131,7 @@ class Trojan_Detector:
             pass
         return 0
 
-    def detect(self, file_path):
-        # 判断传参
-        if len(file_path) == 0:
-            file_path = self.test_path
-
+    def detect(self):
         filename = 'other.abc'
         # 逐包读取
         for msg in self.MQ_Traffic:
@@ -172,26 +167,25 @@ class Trojan_Detector:
                     print(pkt)
                     ftp_data = pkt.layers[9]
                     # 保存ftp文件到test目录下
-                    writefile(filename, ftp_data, file_path)
+                    writefile(filename, ftp_data)
 
-                # checkTrojan
-                checkfile = file_path+filename
-                res = self.checkTrojan(checkfile)
-                if res == 1:
-                    # 发送消息到事件队列
-                    event = AbnormalFlowModel(
-                        type=FLOW_TYPE_TROJAN,
-                        time=datetime.now(),
-                        src=src_ip,
-                        dst=dst_ip,
-                        detail=copy.deepcopy(pkt))
-                    message = pickle.dumps(event)
-                    self.MQ_Event.send(self.MQ_Event_Topic, message)
+                    # checkTrojan
+                    res = self.checkTrojan(filename)
+                    if res == 1:
+                        # 发送消息到事件队列
+                        event = AbnormalFlowModel(
+                            type=FLOW_TYPE_TROJAN,
+                            time=datetime.now(),
+                            src=src_ip,
+                            dst=dst_ip,
+                            detail=copy.deepcopy(pkt))
+                        message = pickle.dumps(event)
+                        self.MQ_Event.send(self.MQ_Event_Topic, message)
 
-                # 删除文件
-                os.remove(checkfile)
-                # 修改filename为默认值
-                filename = 'other.abc'
+                    # 删除文件
+                    os.remove(filename)
+                    # 修改filename为默认值
+                    filename = 'other.abc'
 
 
 
@@ -490,7 +484,7 @@ def get_elf_info(elf, label):
 
 # utility function to clean the dataset by generating unique numeric value for the string values in the dataset
 def get_unique_mappings(feature):
-    clean_data = pd.read_csv('./Trojan/Data/trojan_reordered.csv', low_memory=False)
+    clean_data = pd.read_csv('./abnomal_traffic/trojan/Data/trojan_reordered.csv', low_memory=False)
     tmplist = (sorted(clean_data[feature].unique().tolist()))
     tmpdict = {k: (v + 1) for v, k in enumerate(tmplist)}
     return tmpdict
@@ -567,7 +561,7 @@ def clean_dataset(label):
                      'section_shstrtab_sh_offset', 'section_shstrtab_sh_size', 'section_shstrtab_sh_link',
                      'section_shstrtab_sh_info', 'section_shstrtab_sh_addralign', 'section_shstrtab_sh_entsize']
 
-    given_file = './Trojan/Data/%s.csv' % label
+    given_file = './abnomal_traffic/trojan/Data/%s.csv' % label
     given_data = pd.read_csv(given_file, low_memory=False)
     given_data_columns_list = []
     for i in given_data.columns.values:
@@ -581,15 +575,15 @@ def clean_dataset(label):
             # print("{} was not present, removing it from table...".format(feature))
             given_data = given_data.drop(feature, axis=1)
 
-    given_data.to_csv('./Trojan/Data/trojan_modified.csv', index=False)
-    with open('./Trojan/Data/trojan_modified.csv', 'r') as infile, open('./Trojan/Data/trojan_reordered.csv', 'w',
+    given_data.to_csv('./abnomal_traffic/trojan/Data/trojan_modified.csv', index=False)
+    with open('./abnomal_traffic/trojan/Data/trojan_modified.csv', 'r') as infile, open('./abnomal_traffic/trojan/Data/trojan_reordered.csv', 'w',
                                                                         newline='') as outfile:
         fieldnames = features_list
         writer = csv.DictWriter(outfile, fieldnames=fieldnames)
         writer.writeheader()
         for row in csv.DictReader(infile):
             writer.writerow(row)
-    clean_data = pd.read_csv('./Trojan/Data/trojan_reordered.csv')
+    clean_data = pd.read_csv('./abnomal_traffic/trojan/Data/trojan_reordered.csv')
 
     clean_data['has_dwarf_info'] = clean_data['has_dwarf_info'].replace({True: 1, False: 0})
     clean_data['dwarf_info_config_machine_arch'] = clean_data['dwarf_info_config_machine_arch'].replace(
@@ -843,16 +837,18 @@ def clean_dataset(label):
 
     clean_data.drop_duplicates(inplace=True)  # drop the duplicate lines
 
-    clean_data.to_csv('./Trojan/Data/%s.csv' % label, index=False)  # save the cleaned data to perfect.csv
+    clean_data.to_csv('./abnomal_traffic/trojan/Data/%s.csv' % label, index=False)  # save the cleaned data to perfect.csv
 
-def writefile(filename,ftp_data,file_path):
-    with open(file_path + filename, 'w') as f:
+
+def writefile(filename, ftp_data):
+    tmpfile = filename
+    with open(tmpfile, 'w') as f:
         f.write(str(ftp_data))
     print("successfully write my file")
 
     # 读取文件 调整内容
     lines = []
-    with open(file_path + filename, 'w') as f:
+    with open(tmpfile, 'r') as f:
         f.readline()
         line = f.readline()
         if line:
@@ -863,18 +859,14 @@ def writefile(filename,ftp_data,file_path):
         for line in f:
             line = line.strip("\t")
             line = line[:-3]
-            print("第n行", line)
             lines.append(line)
 
     # 创建新文件并写入内容
-    with open(file_path+'new_data.txt', 'w') as f:
+    with open('new_data.txt', 'w') as f:
         for line in lines:
             f.write(line + "\n")
 
     # 删除原文件
-    os.remove(file_path + filename)
-    # 修改新文件名称为原文件名
-    if os.path.exists(file_path + filename):
-        # 删除文件
-        os.remove(file_path + filename)
-    os.rename(file_path + 'new_data.txt', filename)
+    os.remove(tmpfile)
+
+    os.rename('new_data.txt', tmpfile)
